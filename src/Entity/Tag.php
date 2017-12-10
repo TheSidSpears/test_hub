156a0fc6650a6134c07e1b4bfe9fbbc3f857c8b8
Artur Grigorev <zeroly@ya.ru>
2017-12-10 23:41:35 +0300

    Commit includes 'forgotten' files :(

---
+++ src/Entity/Tag.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TagRepository")
+ */
+class Tag
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *     pattern="^[a-z]+[ ]*[a-z]+$",
+     *     message="Tag must be consisting of alphabetic character and space"
+     * )
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Test", mappedBy="tags")
+     */
+    private $tests;
+
+    public function __construct()
+    {
+        $this->tests = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = strtolower($name);
+    }
+
+    /**
+     * @return ArrayCollection|Tag[]
+     */
+    public function getTests()
+    {
+        return $this->tests;
+    }
+
+    public function addTest(Test $test)
+    {
+        if ($this->tests->contains($test)) {
+            return;
+        }
+
+        $this->tests[] = $test;
+        // not needed for persistence, just keeping both sides in sync
+        $test->addTag($this);
+    }
+
+    public function removeTest(Test $test)
+    {
+        $this->tests->removeElement($test);
+        // not needed for persistence, just keeping both sides in sync
+        $test->removeTag($this);
+    }
+}
---
+++ src/Migrations/Version20171208231923.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171208231923 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE test_tag (test_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_7AF46B441E5D0459 (test_id), INDEX IDX_7AF46B44BAD26311 (tag_id), PRIMARY KEY(test_id, tag_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE test_tag ADD CONSTRAINT FK_7AF46B441E5D0459 FOREIGN KEY (test_id) REFERENCES test (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE test_tag ADD CONSTRAINT FK_7AF46B44BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE test_tag DROP FOREIGN KEY FK_7AF46B44BAD26311');
+        $this->addSql('DROP TABLE tag');
+        $this->addSql('DROP TABLE test_tag');
+    }
+}
