35ff406b3d654727ccb340b9f63943a86ae1cdce
Artur Grigorev <zeroly@ya.ru>
2017-12-11 03:14:01 +0300

    Connect bootstrap via yarp But now bootstrap run from url in template

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // the project directory where compiled assets will be stored
+// the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
@@ -11,14 +11,16 @@ Encore
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+     .addEntry('js/app', './assets/js/app.js')
+     .addStyleEntry('css/app', './assets/css/app.scss')
 
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
+// uncomment if you use Sass/SCSS files
+.enableSassLoader(function(sassOptions) {}, {
+    resolve_url_loader: false
+     })
 
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+// uncomment for legacy applications that require $/jQuery as a global variable
+ .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
